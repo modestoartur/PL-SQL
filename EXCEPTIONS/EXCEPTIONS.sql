5b51bb2b31791b404364bff22088d0582d13c0af
Artur Modesto <modestoartur@gmail.com>
2017-06-06 18:47:32 -0300

    Exception Comentários

--- EXCEPTIONS/EXCEPTIONS.sql
+++ EXCEPTIONS/EXCEPTIONS.sql
@@ -1,15 +1,19 @@
+-- Habilitar o uso do dbms_output
 set serveroutput on;
+
+-- Declaracao de variaveis
 declare
 	v_idade	number;
 begin
 	v_idade:='zzzzz';
-exception
-	when others then
-		raise_application_error(-20001,'Erro desconhecido =>'||
-		sqlerrm);
+	--Obrigatorio 
+	exception
+		when others then
+			raise_application_error(-20001,'Erro desconhecido =>'|| sqlerrm);
 end;
+-- 
 
-
+--ESTRUTURA DE COMANDO
 declare
 	v_erro_numero_par	exception;
 begin
@@ -18,16 +22,17 @@ begin
 			if mod(x,2)=0 then	
 				raise v_erro_numero_par;
 			end if;
+		-- exception criada
 		exception
 			when v_erro_numero_par then
 				dbms_output.put_line (x|| ' é par');
 				raise_application_error (-20999,' Erro de negócio.');
 		end;
 	end loop;
+
+--Obrigatorio 
 exception
 	when others then	
 		raise_application_error(-20001,'Abortou o programa pelo erro'
 		||sqlcode);
 end;
-
-
